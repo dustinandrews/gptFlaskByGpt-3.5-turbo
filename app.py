7754c40fde5ab02ca18f803ccf3c36811c56d1fc
Dustin Andrews <dustinandrews@hotmail.com>
2023-03-18 19:05:23 -0700

    initial

---
+++ app.py
@@ -0,0 +1,60 @@
+import os
+import json
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+import csv
+import base64
+import sys
+import traceback
+from lib import DictArrayManager
+
+app = Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+openai.api_key = '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'
+
+messages = DictArrayManager()
+messages.add("system", "You are a helpful assistant")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        model = request.form["model"]
+        messages.add("user", request.form["query"])
+        try:
+            response = openai.ChatCompletion.create(model=model, messages=messages.array)
+            repsonse_message = response.choices[0].message["content"]
+        except Exception as e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            repsonse_message = "ERROR:\n" + (''.join('!! ' + line for line in lines))
+        messages.add("assistant", repsonse_message)
+        return redirect(url_for('index', _anchor='query'))
+
+    return render_template("index.html", anchor='query', messages=messages.array, tokens=messages.tokens, n=len(messages.array))
+
+
+@app.route('/log')
+def view_log():
+    log_lines = []
+    with open('log.csv', newline='') as log_file:
+        csv_reader = csv.DictReader(log_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in csv_reader:
+            for i in row:
+                if is_base64(row[i]):
+                    row[i] = base64.b64decode(row[i]).decode("utf-8")
+            log_lines.append(row)
+    return render_template('log.html', log_lines=log_lines)
+
+def is_base64(s):
+    try:
+        # Trying to decode the string using base64 decoding
+        decoded_string = base64.b64decode(s).decode("utf-8")
+        # Checking if the decoded string matches the original string
+        if encode(decoded_string) == s:
+            return True
+        else:
+            return False
+    except:
+        return False
---
+++ lib.py
@@ -0,0 +1,67 @@
+import csv
+import base64
+import tiktoken
+
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+class DictArrayManager:
+	def __init__(self):
+		self.array = []
+		self.tokens = []
+		self.logfile = "log.csv"
+
+	def clear(self):
+		self.array = []
+
+	def add(self, role, content):
+		self.array.append({'role': role, 'content': content})
+		num_tokens = len(encoding.encode(role + " " + content))
+		self.tokens.append(num_tokens)
+		self.log_latest()
+
+	def unshift(self, role, content):
+		self.array.insert(0, {'role': role, 'content': content})
+
+	def shift(self, num):
+		if num > len(self.array):
+			self.clear()
+		else:
+			self.array = self.array[num:]
+
+	def count(self):
+		count = 0
+		for item in self.array:
+			for value in item.values():
+				count += len(value.split())
+		return count
+
+	def truncate(self, num):
+		while self.count() > num:
+			self.array = self.array[1:]
+
+	def serialize(self):
+		import json
+		return json.dumps(self.array)
+
+	def deserialize(self, json_string):
+		import json
+		try:
+			deserialized = json.loads(json_string)
+			for item in deserialized:
+				if set(item.keys()) != {'role', 'content'}:
+					raise ValueError("Invalid dictionary format")
+			self.array = deserialized
+		except json.JSONDecodeError:
+			raise ValueError("Invalid JSON string format")
+
+	def log_latest(self):
+		latest = self.array[-1]
+		with open(self.logfile, mode='a', newline='') as log_file:
+			csv_writer = csv.writer(log_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			csv_writer.writerow([latest["role"], self.encode(latest["content"])])
+
+	def encode(self, data):
+		content_bytes = data.encode("utf-8")
+		base64_byte = base64.b64encode(content_bytes)
+		base64_string = base64_byte.decode("utf-8")
+		return base64_string
